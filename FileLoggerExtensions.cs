cfc649dc5acfc4505315ac70f5f47038b7504e0e
nikhilvjoshi <65969528+nikhilvjoshi@users.noreply.github.com>
2025-01-24 23:22:35 +0530

    first commit for project to upload files on Google Drive using Google Drive APIs (#1)
    Co-authored-by: Nikhil Joshi <nikhil.joshi@autodesk.com>

---
+++ FileLoggerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+namespace DailyTaskService
+{
+    internal static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory, string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
---
+++ GoogleDriveUploader.cs
@@ -0,0 +1,40 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace DailyTaskService
+{
+    internal class GoogleDriveUploader
+    {
+        static string[] Scopes = { DriveService.Scope.DriveFile };
+        static string ApplicationName = "{{Google Drive Application Name}}";
+        static DriveService service;
+
+        public static DriveService Authenticate()
+        {
+            UserCredential credential;
+
+            // Load client secrets.
+            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file client_secrets.json should be the downloaded JSON from Google Cloud Console.
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore("Drive.Api.Store")).Result;
+            }
+
+            // Create the Drive API service.
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            return service;
+        }
+    }
+}
